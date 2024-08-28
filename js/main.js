179f88f89e650d63ad9c063bd68fdad5682d6101
Наталья <alinatali19@icloud.com>
2024-08-28 21:40:40 +0300

    added adaptive header

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+});
 
+const menuBtn = document.querySelector(".menu__btn");
+const headerNav = document.querySelector(".header__nav");
+const overlay = document.querySelector(".overlay");
+
+menuBtn.addEventListener("click", () => {
+  headerNav.classList.toggle("header__nav--open");
+  overlay.classList.toggle("header__nav--open");
 });
